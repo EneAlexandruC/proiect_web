fee97ffc6a74923a093cad4a0531c4e81024bafb
akel <ene.cristian.p7q@student.ucv.ro>
2024-05-10 02:22:06 +0300

    modificari

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
-import Introduction from "./homepage/Introduction";
 import NavigationBar from "./navi/Navbar";
 import { Container, Row, Col } from "react-bootstrap";
 import backgroundImage from "./assets/background-player-on-right.jpg";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import "./App.css";
+import Sectii from "./sectii/Sectii";
+import Introduction from "./homepage/Introduction";
 
 function App() {
   const containerStyle = {
@@ -13,11 +15,17 @@ function App() {
     minHeight: "100vh",
     width: "100%",
   };
+
   return (
-    <div fluid style={containerStyle}>
-      <NavigationBar />
-      <Introduction />
-    </div>
+    <Router>
+      <div fluid="true" style={containerStyle}>
+        <NavigationBar />
+        <Routes>
+          <Route path="/" element={<Introduction />} />
+          <Route path="/sectii" element={<Sectii />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
--- src/navi/Navbar.jsx
+++ src/navi/Navbar.jsx
@@ -1,14 +1,14 @@
 import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
-import NavDropdown from "react-bootstrap/NavDropdown";
 import logo from "../assets/stema.png";
+import { Link } from "react-router-dom";
 
 function NavigationBar() {
   return (
     <Navbar collapseOnSelect expand="lg" className="custom-navbar">
       <Container style={{ marginLeft: "5rem" }}>
-        <Navbar.Brand href="#home">
+        <Navbar.Brand href="/">
           <img
             src={logo}
             alt="Logo"
@@ -20,27 +20,21 @@ function NavigationBar() {
         <Navbar.Toggle aria-controls="responsive-navbar-nav" />
         <Navbar.Collapse id="responsive-navbar-nav">
           <Nav className="me-auto" style={{ paddingLeft: "0" }}>
-            <Nav.Link href="#features" className="nav-link">
-              Features
+            <Nav.Link as={Link} to="/sectii" className="nav-link">
+              Sectii
             </Nav.Link>
-            <Nav.Link href="#pricing" className="nav-link">
-              Pricing
+            <Nav.Link as={Link} to="/echipe" className="nav-link">
+              Echipe
             </Nav.Link>
-            <NavDropdown title="Dropdown" id="collapsible-nav-dropdown">
-              <NavDropdown.Item href="#action/3.1" className="nav-link">
-                Action
-              </NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.2" className="nav-link">
-                Another action
-              </NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.3" className="nav-link">
-                Something
-              </NavDropdown.Item>
-              <NavDropdown.Divider />
-              <NavDropdown.Item href="#action/3.4" className="nav-link">
-                Separated link
-              </NavDropdown.Item>
-            </NavDropdown>
+            <Nav.Link as={Link} to="/anunturi" className="nav-link">
+              Anunturi
+            </Nav.Link>
+            <Nav.Link as={Link} to="/galerie" className="nav-link">
+              Galerie
+            </Nav.Link>
+            <Nav.Link as={Link} to="/contact" className="nav-link">
+              Contact
+            </Nav.Link>
           </Nav>
         </Navbar.Collapse>
       </Container>
